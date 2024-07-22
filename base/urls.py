1c7452d64e824d5da69074d257c83b69ccf7bcfc
criSiles <cristina.siles.vega@gmail.com>
2024-07-22 18:34:33 +0200

    feat: Add base app with models, tests, admin, views, and templates
    This commit adds the base app to the project. It includes the following changes:
    - Added views.py file with initial code for rendering home and room pages.
    - Added templates for navbar, home, and room pages.
    - Updated urls.py file to include the base app's URLs.

---
+++ base/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path 
+# urls and views are in the same directory so we're going to import the views to access the functions to route
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('room/<str:pk>', views.room, name="room")
+]
