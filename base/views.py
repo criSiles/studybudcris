0638aaabc0078b82aa4780bf813e525b74813e9f
criSiles <cristina.siles.vega@gmail.com>
2024-07-24 17:31:42 +0200

    feat: Add delete functionality for rooms.

--- base/views.py
+++ base/views.py
@@ -13,17 +13,20 @@ from .forms import RoomForm
 #     {'id': 3, 'name': 'Frontend Development'}, 
 # ]
 
+# HOME VIEW
 def home(request):
     # this Room.objects.all() is used to get all the rooms from the database
     rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
+# ROOM VIEW
 def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
+# CREATE ROOM VIEW
 def createRoom(request):
     form = RoomForm()
     if request.method == 'POST':
@@ -37,6 +40,7 @@ def createRoom(request):
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
+# UPDATE ROOM VIEW
 # To update a room we need to pass the primary key of the room
 def updateRoom(request, pk):
     # This is going to get the room that we want to update from the database
@@ -47,7 +51,18 @@ def updateRoom(request, pk):
         # This is going to rewrite the form with the new data
         form = RoomForm(request.POST, instance=room)
         if form.is_valid():
+            # This is going to save the form in the database
             form.save()
             return redirect('home')
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+# DELETE ROOM VIEW
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        # This is going to delete the room from the database
+        room.delete()
+        return redirect('home')
+    # I pass the obj key like this to make it reusable in the delete.html template
+    return render(request, 'base/delete.html', {'obj':room})
